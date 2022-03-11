e721069b20cfe940b35ee422f2dcfa343660c0eb
Dave Redfern <david.redfern@madagasgar.com>
2022-03-11 13:55:16 -0500

    Try to add CPU temp

--- README.md
+++ README.md
@@ -2,9 +2,8 @@
 
 Forked from https://github.com/gigawatts/cockpit-temperature-plugin to add NVMe drive temperatures.
 
-__Note:__ this is customised for my own drive layout / setup. CPU temps are disabled as Fedora 34
-currently does not have the revised AMD Epyc temperature monitoring profiles (at least right now
-on kernel 5.13.5-200.fc34.x86_64) so lm_sensors does not report core temps.
+__Note:__ this is customised for my own drive layout / setup. Only total CPU temp is available
+due to missing monitoring profiles.
 
 Cockpit Temperature Plugin using [smoothie-charts](http://smoothiecharts.org)
 
--- temperatures.js
+++ temperatures.js
@@ -11,23 +11,24 @@ window.onload = function () {
             tooltip: true
         }),
         canvas = document.getElementById('temps'),
-        seriesData = [new TimeSeries(), new TimeSeries(), new TimeSeries(), new TimeSeries(), new TimeSeries()];
+        seriesData = [new TimeSeries(), new TimeSeries(), new TimeSeries(), new TimeSeries(), new TimeSeries(), new TimeSeries()];
 
     chart.addTimeSeries(seriesData[0], {lineWidth: 2, strokeStyle: '#ff0000'});
     chart.addTimeSeries(seriesData[1], {lineWidth: 2, strokeStyle: '#00ffff'});
     chart.addTimeSeries(seriesData[2], {lineWidth: 2, strokeStyle: '#ffff00'});
     chart.addTimeSeries(seriesData[3], {lineWidth: 2, strokeStyle: '#f000f0'});
     chart.addTimeSeries(seriesData[4], {lineWidth: 2, strokeStyle: '#00ff00'});
+    chart.addTimeSeries(seriesData[5], {lineWidth: 2, strokeStyle: '#0000FF'});
     chart.streamTo(canvas, 500);
 
-    function get_cpu_temp(series, core) {
-        let proc = cockpit.script("sensors | grep 'Core " + core + ":' | awk '{print $3}'");
+    function get_cpu_temp(series) {
+        let proc = cockpit.script("sensors k10temp-pci-00c3 | grep \"Tctl:\" | grep -oP \"([\d.]+)\" | xargs");
         proc.done(function (data) {
-            pt = parseFloat(data.match(/([0-9\.]+)/)[1]);
+            pt = parseFloat(data.match(/([0-9.]+)/)[1]);
             series.append(new Date().getTime(), pt);
-            document.getElementById("cpu" + core + "_temp").innerHTML = pt;
+            document.getElementById("cpu_temp").innerHTML = pt;
         });
-    };
+    }
 
     function get_hdd_temp(series, drive) {
         let hdd = cockpit.script("hddtemp -n " + drive);
@@ -36,14 +37,15 @@ window.onload = function () {
             series.append(new Date().getTime(), pt);
             document.getElementById("hdd" + drive.replace(/\//g, '_') + "_temp").innerHTML = pt;
         });
-    };
+    }
 
     setInterval(function () {
-        get_hdd_temp(seriesData[0], '/dev/nvme0n1')
-        get_hdd_temp(seriesData[1], '/dev/nvme1n1')
-        get_hdd_temp(seriesData[2], '/dev/nvme2n1')
-        get_hdd_temp(seriesData[3], '/dev/nvme3n1')
-        get_hdd_temp(seriesData[4], '/dev/nvme4n1')
+        get_cpu_temp(seriesData[0])
+        get_hdd_temp(seriesData[1], '/dev/nvme0n1')
+        get_hdd_temp(seriesData[2], '/dev/nvme1n1')
+        get_hdd_temp(seriesData[3], '/dev/nvme2n1')
+        get_hdd_temp(seriesData[4], '/dev/nvme3n1')
+        get_hdd_temp(seriesData[5], '/dev/nvme4n1')
     }, 2000);
 }
 
